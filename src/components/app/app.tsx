9d950cc85595c74fbfc0ab364b2a22104038bd7d
Daniil Osiptsov <posikunets@yandex.ru>
2025-01-09 16:24:29 +0300

    install react-router-dom, add routes in app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,112 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { ProtectedRoute } from '../protected-route/protected-route';
+import { Provider } from 'react-redux';
+import store from '../../services/store';
 
 const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
+  <Provider store={store}>
+    <div className={styles.app}>
+      <AppHeader />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed'>
+            <Route index element={<Feed />} />
+            <Route
+              path=':number'
+              element={
+                <Modal title='order' onClose={() => console.log('close')}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+          </Route>
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='order' onClose={() => console.log('close')}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/login'
+            element={
+              <ProtectedRoute>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/register'
+            element={
+              <ProtectedRoute>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/forgot-password'
+            element={
+              <ProtectedRoute>
+                <ForgotPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/reset-password'
+            element={
+              <ProtectedRoute>
+                <ResetPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route path='/profile'>
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='orders'
+              element={
+                <ProtectedRoute>
+                  <ProfileOrders />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='orders/:number'
+              element={
+                <ProtectedRoute>
+                  <OrderInfo />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  </Provider>
 );
 
 export default App;
