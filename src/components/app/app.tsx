d6d9fc26b0289ea9dc484b0c7855ddd38e49ac4d
Osiptsov Danil <posikunets999@gmail.com>
2025-01-11 21:06:27 +0300

    write routes in app.jsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -13,100 +13,122 @@ import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import { ProtectedRoute } from '../protected-route/protected-route';
-import { Provider } from 'react-redux';
-import store from '../../services/store';
+// import { useEffect } from 'react';
+// import { useDispatch } from '../../services/store';
+// import { requestIngredients } from '../../slices/rootSlice';
+// import { requestAllOrders } from '../../slices/orderSlice';
 
-const App = () => (
-  <Provider store={store}>
+const App = () => {
+  const navigate = useNavigate();
+  // const dispatch = useDispatch();
+  const location = useLocation();
+  const backgroundLocation = location.state?.background;
+
+  // useEffect(() => {
+  //   dispatch(requestIngredients());
+  //   dispatch(requestAllOrders());
+  // }, []);
+
+  const onCloseModal = () => {
+    navigate(backgroundLocation);
+  };
+
+  return (
     <div className={styles.app}>
       <AppHeader />
-      <BrowserRouter>
+      <Routes location={backgroundLocation || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route
+          path='/login'
+          element={
+            <ProtectedRoute OnlyOnAuth>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <ProtectedRoute OnlyOnAuth>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/forgot-password'
+          element={
+            <ProtectedRoute OnlyOnAuth>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/reset-password'
+          element={
+            <ProtectedRoute>
+              <ResetPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='/profile'>
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='orders'
+            element={
+              <ProtectedRoute>
+                <ProfileOrders />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='orders/:number'
+            element={
+              <ProtectedRoute>
+                <OrderInfo />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+      {backgroundLocation && (
         <Routes>
-          <Route path='/' element={<ConstructorPage />} />
-          <Route path='/feed'>
-            <Route index element={<Feed />} />
-            <Route
-              path=':number'
-              element={
-                <Modal title='order' onClose={() => console.log('close')}>
-                  <OrderInfo />
-                </Modal>
-              }
-            />
-          </Route>
           <Route
             path='/ingredients/:id'
             element={
-              <Modal title='order' onClose={() => console.log('close')}>
+              <Modal title='order' onClose={onCloseModal}>
                 <IngredientDetails />
               </Modal>
             }
           />
-          <Route
-            path='/login'
-            element={
-              <ProtectedRoute>
-                <Login />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/register'
-            element={
-              <ProtectedRoute>
-                <Register />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/forgot-password'
-            element={
-              <ProtectedRoute>
-                <ForgotPassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/reset-password'
-            element={
-              <ProtectedRoute>
-                <ResetPassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route path='/profile'>
-            <Route
-              index
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='orders'
-              element={
-                <ProtectedRoute>
-                  <ProfileOrders />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='orders/:number'
-              element={
-                <ProtectedRoute>
-                  <OrderInfo />
-                </ProtectedRoute>
-              }
-            />
-          </Route>
-          <Route path='*' element={<NotFound404 />} />
         </Routes>
-      </BrowserRouter>
+      )}
+      {backgroundLocation && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='order' onClose={onCloseModal}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
     </div>
-  </Provider>
-);
+  );
+};
 
 export default App;
